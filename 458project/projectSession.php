36ff65275d65d6a6f6a19e254c03298cf27daf2b
Chad <cjd10@humboldt.edu>
2018-11-07 10:55:13 -0800

    Added Test_case.php and edited the main page

--- 458project/projectSession.php
+++ 458project/projectSession.php
@@ -25,6 +25,7 @@ http://nrs-projects.humboldt.edu/~cjd10/458project/projectSession.php
 
     <?php
     require_once("create_login.php");
+    require_once("create_test.php");
     require_once("hsu_conn_sess.php");
     require_once("destroy_and_exit.php");
     ?>
@@ -41,7 +42,21 @@ http://nrs-projects.humboldt.edu/~cjd10/458project/projectSession.php
        {
            //Creates Loogin form
            create_login();
+	   $_SESSION['next-stage'] = "test_data";
        }
+    elseif ($_SESSION['next-stage'] == "test_data")
+       {
+	   //Creates Form to test data in database
+	   create_test();
+       	   
+	   session_destroy();
+	   session_regenerate_id(TRUE);
+	   session_start();
+
+	   create_login();
+	   $_SESSION['next-stage'] = "create_login";
+       }
+    
 
     else
     {
@@ -55,7 +70,7 @@ http://nrs-projects.humboldt.edu/~cjd10/458project/projectSession.php
         session_start();
 
          create_login();
-        $_SESSION['next-stage'] = "create_options";
+        
     }
 
      
