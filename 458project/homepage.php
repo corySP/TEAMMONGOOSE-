ac7bed10b94db030fa87e73c8d329d5e9968ff47
cory <cjs781@humboldt.edu>
2018-11-19 16:20:23 -0800

    basic structure of homepage/mainpage :)

---
+++ 458project/homepage.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+
+<!--
+    cory sprague
+    lm: 11/19/18
+-->
+
+<head>
+	<title> WEASELCHAT </title>
+        <meta charset="utf-8" />
+	<link rel="stylesheet" type="text/css" href="homepage.css">
+</head>
+
+<body>
+        <input type="button" onclick="location.href='...'" value="SETTINGS" />
+	<input type="button" onclick="location.href='...'" value="CALENDAR" />
+	<input type="button" onclick="location.href='...'" value="FILEVIEW" />
+	
+	<h1> WEASELCHAT </h1>	
+	<hr />
+	<div class="groupbar">
+		<a href="#group1">Group 1</a>	
+		<a href="#group2">Group 2</a>
+		<a href="#group3">Group 3</a>
+	</div>
+	
+	<div class="main">
+		<h2> Welcome to the weasel/mongoose file chat calendar scheduling website!
+</h2>
+
+	</div>
+	<div class="chatbar">
+	</div>
+</body>
+</html>
