a07eefadf03c8a450d437bb4494e57bd800fe9b9
Mabel Houle <mabelhoule777@gmail.com>
2018-11-28 12:36:34 -0800

    debu

--- 458project/user_calendar_page.php
+++ 458project/user_calendar_page.php
@@ -10,6 +10,7 @@
 
      <?php
       require_once("hsu_conn_sess.php");
+      require("../../_conn.php");
      ?>
 
      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
