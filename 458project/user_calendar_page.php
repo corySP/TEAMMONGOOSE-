c8b3f61c645455e0fbe10f820fe930ab5285f863
Mabel Houle <mabelhoule777@gmail.com>
2018-11-28 01:55:08 -0800

    debug

--- 458project/user_calendar_page.php
+++ 458project/user_calendar_page.php
@@ -132,5 +132,8 @@ $old_error_handler = set_error_handler("ErrorHandler");
  	echo $js_tasks;
  	echo $js_events;
 ?>
+
+     <script src="calendar.js" type="text/javascript"></script>
+     
 </body>
 </html>
