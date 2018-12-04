99793374ee8a8540006fc115096c16fad2706245
Mabel Houle <mabelhoule777@gmail.com>
2018-12-04 00:08:36 -0800

    integratio

--- 458project/create_login.php
+++ 458project/create_login.php
@@ -1,9 +1,6 @@
 <?php
-
-
 /*=====
     Starting STUB
-
 function create_login()
 {
     ?>
@@ -11,49 +8,24 @@ function create_login()
     <?php
 }
 =====*/
-
 /*=====
     function: create_login: void -> void
     purpose: expects nothing, returns nothing, and has the side-effect
         of outputting to the resulting document an Oracle log-on form
         with method="post" and action equal to the calling PHP document
-
     requires: name-pwd-fieldset.html, 328footer.html
 =====*/
-
 function create_login()
 {
     if ( (!array_key_exists("confirmation-submit", $_POST) &&
          (!array_key_exists("user_log_out", $_POST)) &&
-	 (!array_key_exists("register-back", $_POST))) )
+	  (!array_key_exists("register-back", $_POST))) )
      {   
-<<<<<<< HEAD
-	 if ( (! array_key_exists("username", $_POST)) or
-         (! array_key_exists("password", $_POST)) or
-         ($_POST["username"] == "") or
-         ($_POST["password"] == "") or
-         (! isset($_POST["username"])) or
-         (! isset($_POST["password"])) )
-   	    {
-              destroy_and_exit("must enter a username and password!");
-   	    }
-
-    $_SESSION["current_user"] = "none";    
-
-    $master_username = strip_tags($_POST["username"]);
-    $master_password = strip_tags($_POST["password"]);
-
-    $_SESSION["master_username"] = $master_username;
-    $_SESSION["master_password"] = $master_password;
-
-=======
-   	   
+        
     $_SESSION["current_user"] = "none";  
-
     require_once("../../_conn.php");  
     $_SESSION["master_username"] = DB_USER;
     $_SESSION["master_password"] = DB_PASS;
->>>>>>> 06057d091fa62a7d411066d7ec4fba8194e39174
     }
     // create the desired Oracle log-in form
     ?>
@@ -61,14 +33,6 @@ function create_login()
               action="<?= htmlentities($_SERVER['PHP_SELF'],
                                        ENT_QUOTES) ?>">
         <fieldset>
-<<<<<<< HEAD
-        <?php
-        require_once("name-pwd-fieldset.html");
-        ?>
-            <input type="submit" id="register-button" name="register-button" value="Register" formnovalidate />
-            <input type="submit" id="submit-button" name="login-submit-button"  value="login" />
-	    <input type="submit" id="exit-button" name="login-exit-button" value="Exit" formnovalidate />
-=======
 	 <fieldset>
          <legend> Enter your username/password:
          </legend>
@@ -83,12 +47,8 @@ function create_login()
           </fieldset>
             <input type="submit" id="register-button" name="register-button" value="Register" formnovalidate />
             <input type="submit" id="submit-button" name="login-submit-button"  value="login" />
->>>>>>> 06057d091fa62a7d411066d7ec4fba8194e39174
         </fieldset>
         </form>
         <?php
 }
-
-
-
 ?>
